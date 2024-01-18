3ba9f9d65222084eb3242b464ada6224256b6489
hansanhha <kwixs35@gmail.com>
2024-01-18 15:10:35 +0900

    first commit

---
+++ boot/src/test/java/com/hansanhha/spring/boot/BootApplicationTest.java
@@ -0,0 +1,12 @@
+package com.hansanhha.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BootApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ datajpa/src/test/java/com/hansanhha/spring/DataJpaApplicationTest.java
@@ -0,0 +1,15 @@
+package com.hansanhha.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DataJpaApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
