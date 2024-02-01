539b362bd40aa6c2f9b3fcda4b89ec806c97cd07
hansanhha <kwixs35@gmail.com>
2024-02-01 13:14:33 +0900

    [gradle] 커스텀 자바 플러그인 수정

--- buildSrc/src/main/java/com/hansanhha/spring/JavaPlugin.java
+++ buildSrc/src/main/java/com/hansanhha/spring/JavaPlugin.java
@@ -2,7 +2,9 @@ package com.hansanhha.spring;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.internal.tasks.testing.junitplatform.JUnitPlatformSpec;
 import org.gradle.api.plugins.JavaPluginExtension;
+import org.gradle.api.tasks.testing.Test;
 import org.gradle.jvm.toolchain.JavaLanguageVersion;
 
 public class JavaPlugin implements Plugin<Project> {
@@ -18,6 +20,8 @@ public class JavaPlugin implements Plugin<Project> {
         project.getExtensions().configure(JavaPluginExtension.class, javaPlugin -> {
             javaPlugin.toolchain(spec -> spec.getLanguageVersion().set(JavaLanguageVersion.of(21)));
         });
+
+        project.getTasks().withType(Test.class).configureEach(Test::useJUnitPlatform);
 
         project.getDependencies().add(COMPILE_ONLY, LOMBOK);
         project.getDependencies().add(ANNOTATION_PROCESSOR, LOMBOK);
